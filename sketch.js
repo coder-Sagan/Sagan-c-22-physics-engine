191b73dc9019365ae080670e99315957f20826bb
coder-Sagan <58737041+coder-Sagan@users.noreply.github.com>
2019-12-29 11:48:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+/*const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+var engine,world;
+var object;
+var ground;
+var ball;
+
+function setup(){
+    var canvas=createCanvas(400,400);
+    engine=Engine.create();
+    world=engine.world;
+
+    var ground_options={
+        isStatic:true
+    }
+    ground=Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    var object_options={
+        isStatic:true
+    }
+    object=Bodies.rectangle(200,100,50,50,object_options);
+    World.add(world,object);
+
+    var ball_options={
+        restitution:1.0
+    }
+    ball=Bodies.circle(100,100,50,50,ball_options);
+    World.add(world,ball);
+
+    console.log(object);
+
+}
+ function draw(){
+     background(0);
+     Engine.update(engine);
+     rectMode(CENTER);
+     rect(ground.position.x,ground.position.y,400,20);
+     rect(object.position.x,object.position.y,50,50);
+     ellipseMode(RADIUS);
+     ellipse(ball.position.x,ball.position.y,20,20);
+
+ }*/
+
+   
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, ball;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 5.0
+    }
+
+    ball = Bodies.circle(200,100,20, ball_options);
+    World.add(world,ball);
+
+    console.log(ground);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
+}
